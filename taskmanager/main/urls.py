57a01f342c869672764b0cfdc3fcbd271b6362ec
KonstantinK <k.kostromin@bk.ru>
2021-08-12 18:47:02 +0300

    taskmanager

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='main'),
+    path('about-us', views.about, name='about'),
+    path('create', views.create, name='create'),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+        else:
+            'Форма была не верной'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
